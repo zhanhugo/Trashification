b9878b38579656b0aa809df085745c4807fea63b
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2020-02-08 14:29:52 -0800

    Initial Commit

---
+++ hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hangman
+//
+//  Created by Jackson Chui on 2/8/20.
+//  Copyright © 2020 Tim's Apples. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
